a8412adb8df1c2dd814c47a98b199d7190d18931
Peter Bingham <petersbingham@hotmail.co.uk>
2016-06-18 18:59:16 +0100

    Rename

--- multichannel/urazil_6chaneleastic/matreader.py
+++ multichannel/urazil_6chaneleastic/matreader.py
@@ -7,7 +7,7 @@ FILENAME = "fort.19"
 NUMCHANNELS = 6
 ENEFACTOR = sm.EFROMK_RYDBERGS
 def getFileHandler(kCal, startIndex, endIndex):
-    sysName = "Urazil" + "_" + str(kCal) + "_" + str(startIndex)+ "_" + str(endIndex)
+    sysName = "Urazil_6chanEleastic_" + str(kCal) + "_" + str(startIndex)+ "_" + str(endIndex)
     return ResultFileHandler(sysName)
 
 def readkMats(fileName):
